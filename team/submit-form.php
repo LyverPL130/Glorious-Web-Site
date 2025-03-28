074d5bf77d7c70cd3eb672473526ce60443084a8
lyverPL130 <boris.130@icloud.com>
2025-03-28 21:39:04 +0800

    v0.1.1BETAv0.1 :FormPHP

---
+++ team/submit-form.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "boris.130@icloud.com";
+    $subject = "來自網站的表單提交";
+    $txt = "姓名： " . $name . "\n電子郵件： " . $email . "\n訊息： " . $message;
+    $headers = "From: bs130.glorious@maomy.top"
+
+    mail($to, $subject, $txt, $headers);
+    echo "表單已成功提交！";
+}
+?>
